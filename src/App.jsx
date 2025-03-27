be0d3a6c702b087f992683c197c47731bce403a3
“alerenna” <alessandrorennafm@gmail.com>
2025-03-27 22:54:32 +0800

    Add genre filter that work simultanously with search by title

--- src/App.jsx
+++ src/App.jsx
@@ -12,43 +12,66 @@ const moviesList = [
 function App() {
 
 
-  const [searchQuery, setSearchQuery] = useState('')
+  const [searchMovieQuery, setSearchMovieQuery] = useState('')
   const [filteredMovie, setFilteredMovie] = useState(moviesList)
+  const [selectedGenre, setSelectedGenre] = useState('Select One')
 
   useEffect(() => {
 
-    console.log(moviesList);
+    let filtered = moviesList
+    if (searchMovieQuery.trim() !== '') {
+      filtered = filtered.filter(movie => movie.title.toLowerCase().includes(searchMovieQuery))
+    }
+
+    if (selectedGenre !== 'Select One') {
+      filtered = filtered.filter(movie => movie.genre === selectedGenre)
+    }
+
+    setFilteredMovie(filtered)
 
 
-    setFilteredMovie(moviesList.filter(movie => movie.title.toLowerCase().includes(searchQuery)))
-
-  }, [moviesList, searchQuery])
-
+  }, [moviesList, searchMovieQuery, selectedGenre])
 
 
   return (
 
     <>
-
-
       <div className="fix-container border p-5 rounded">
         <h1 className="text-center">Movies List</h1>
 
         <div className="container">
           <div className="mb-3">
-            <label htmlFor="" className="form-label">Search</label>
+            <label htmlFor="" className="form-label">Search movie by title</label>
             <input
               type="text"
               className="form-control"
-              name="search"
-              id="search"
+              name="searchTitle"
+              id="searchTitle"
               aria-describedby="SearchHelpId"
-              placeholder="Search movie here..."
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
+              placeholder="Search movie by title here..."
+              value={searchMovieQuery}
+              onChange={(e) => setSearchMovieQuery(e.target.value)}
             />
           </div>
         </div>
+
+        <div className="mb-3">
+          <label htmlFor="" className="form-label">Search movie by Genre</label>
+          <select
+            className="form-select"
+            name="selectGenre"
+            id="selectGenre"
+            value={selectedGenre}
+            onChange={(e) => setSelectedGenre(e.target.value)}
+          >
+            <option defaultValue={''}>Select one</option>
+            <option value="Fantascienza">Fantascienza</option>
+            <option value="Thriller">Thriller</option>
+            <option value="Romantico">Romantico</option>
+            <option value="Azione">Azione</option>
+          </select>
+        </div>
+
 
         <div className="container d-flex justify-content-center align-items-center">
           {filteredMovie.map((movie, index) => (
