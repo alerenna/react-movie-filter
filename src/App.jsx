f9ba584dd9566157d0e0fa8b8b46493b4478d8c7
“alerenna” <alessandrorennafm@gmail.com>
2025-03-27 23:52:22 +0800

    Add functionality to add new movies and update the list of filtered movies

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,25 @@ function App() {
   const [searchMovieQuery, setSearchMovieQuery] = useState('')
   const [filteredMovie, setFilteredMovie] = useState(moviesList)
   const [selectedGenre, setSelectedGenre] = useState('Select One')
+  const [newMovieList, setNewMovieList] = useState(moviesList)
+  const [newMovie, setNewMovie] = useState({ title: '', genre: '' })
+
+  function handleFormSubmit(e) {
+    e.preventDefault()
+
+    const updateList = ([...newMovieList, newMovie])
+    setNewMovieList(updateList)
+    setNewMovie({ title: '', genre: '' })
+
+    console.log(newMovieList);
+
+  }
+
+
 
   useEffect(() => {
 
-    let filtered = moviesList
+    let filtered = newMovieList
     if (searchMovieQuery.trim() !== '') {
       filtered = filtered.filter(movie => movie.title.toLowerCase().includes(searchMovieQuery))
     }
@@ -28,17 +43,17 @@ function App() {
     }
 
     setFilteredMovie(filtered)
-
-
-  }, [moviesList, searchMovieQuery, selectedGenre])
+  }, [newMovieList, searchMovieQuery, selectedGenre])
 
 
   return (
 
     <>
       <div className="fix-container border p-5 rounded">
+        {/* Title */}
         <h1 className="text-center">Movies List</h1>
 
+        {/* Title search input */}
         <div className="container">
           <div className="mb-3">
             <label htmlFor="" className="form-label">Search movie by title</label>
@@ -54,31 +69,74 @@ function App() {
             />
           </div>
         </div>
-
-        <div className="mb-3">
-          <label htmlFor="" className="form-label">Search movie by Genre</label>
-          <select
-            className="form-select"
-            name="selectGenre"
-            id="selectGenre"
-            value={selectedGenre}
-            onChange={(e) => setSelectedGenre(e.target.value)}
-          >
-            <option defaultValue={''}>Select one</option>
-            <option value="Fantascienza">Fantascienza</option>
-            <option value="Thriller">Thriller</option>
-            <option value="Romantico">Romantico</option>
-            <option value="Azione">Azione</option>
-          </select>
+        {/* Genre search select */}
+        <div className="container">
+          <div className="mb-3">
+            <label htmlFor="" className="form-label">Search movie by Genre</label>
+            <select
+              className="form-select"
+              name="selectGenre"
+              id="selectGenre"
+              value={selectedGenre}
+              onChange={(e) => setSelectedGenre(e.target.value)}
+            >
+              <option defaultValue={''}>Select one</option>
+              <option value="Fantascienza">Fantascienza</option>
+              <option value="Thriller">Thriller</option>
+              <option value="Romantico">Romantico</option>
+              <option value="Azione">Azione</option>
+            </select>
+          </div>
         </div>
 
-
+        {/* Cicle for print movies on cards */}
         <div className="container d-flex justify-content-center align-items-center">
           {filteredMovie.map((movie, index) => (
             <div key={`move-${index}`} className="card p-3 m-1 mb-3">
               {movie.title}
             </div>
           ))}
+        </div>
+
+        {/* Input for add a movie */}
+        <div className="container">
+          <form className="mb-3" onSubmit={handleFormSubmit}>
+            <div className="mb-3">
+              <label htmlFor="" className="form-label">Add new movie title</label>
+              <input
+                type="text"
+                className="form-control"
+                name="new-movie-title"
+                id="new-movie-title"
+                aria-describedby="newMovieTitleHelpId"
+                placeholder="Insert movie title here"
+                value={newMovie.title}
+                onChange={(e) => setNewMovie({ ...newMovie, title: e.target.value })}
+              />
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="" className="form-label">Add new movie genre</label>
+              <input
+                type="text"
+                className="form-control"
+                name="new-movie-genre"
+                id="new-movie-genre"
+                aria-describedby="newMovieGenreHelpId"
+                placeholder="Insert movie genre here"
+                value={newMovie.genre}
+                onChange={(e) => setNewMovie({ ...newMovie, genre: e.target.value })}
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="btn btn-primary"
+            >
+              Add movie
+            </button>
+
+          </form>
         </div>
 
       </div>
