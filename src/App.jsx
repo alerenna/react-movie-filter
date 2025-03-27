dabd8b59b0e0e6a679861bf5a0af02e0cdc62ffb
“alerenna” <alessandrorennafm@gmail.com>
2025-03-27 21:08:37 +0800

    Refactor movie list display and add search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,66 @@
+import { useState, useEffect } from "react"
 
+const moviesList = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
 
 function App() {
-  const moviesList = [
-    { title: 'Inception', genre: 'Fantascienza' },
-    { title: 'Il Padrino', genre: 'Thriller' },
-    { title: 'Titanic', genre: 'Romantico' },
-    { title: 'Batman', genre: 'Azione' },
-    { title: 'Interstellar', genre: 'Fantascienza' },
-    { title: 'Pulp Fiction', genre: 'Thriller' },
-  ]
+
+
+  const [searchQuery, setSearchQuery] = useState('')
+  const [filteredMovie, setFilteredMovie] = useState(moviesList)
+
+  useEffect(() => {
+
+    console.log(moviesList);
+
+
+    setFilteredMovie(moviesList.filter(movie => movie.title.toLowerCase().includes(searchQuery)))
+
+  }, [moviesList, searchQuery])
+
+
 
   return (
-    <>
-      <h1 className="text-center">Movies List</h1>
 
-      <div className="container d-flex justify-content-center align-items-center flex-column">
-        {moviesList.map(movie => (
-          <div className="card p-3 mb-1 mt-1">
-            {movie.title}
+    <>
+
+
+      <div className="fix-container border p-5 rounded">
+        <h1 className="text-center">Movies List</h1>
+
+        <div className="container">
+          <div className="mb-3">
+            <label htmlFor="" className="form-label">Search</label>
+            <input
+              type="text"
+              className="form-control"
+              name="search"
+              id="search"
+              aria-describedby="SearchHelpId"
+              placeholder="Search movie here..."
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
           </div>
-        ))}
+        </div>
+
+        <div className="container d-flex justify-content-center align-items-center">
+          {filteredMovie.map((movie, index) => (
+            <div key={`move-${index}`} className="card p-3 m-1 mb-3">
+              {movie.title}
+            </div>
+          ))}
+        </div>
+
       </div>
+
+
     </>
   )
 }
